b1f57602f590bc7b839024fcadb21d7218b08ad8
bala750 <122767885+bala750@users.noreply.github.com>
2024-02-09 10:32:50 +0530

    Add files via upload

---
+++ Backend/day13/src/main/java/com/example/springapp/service/UserService.java
@@ -0,0 +1,23 @@
+package com.example.springapp.service;
+
+import java.util.List;
+
+import com.example.springapp.entity.User;
+
+public interface UserService {
+    
+
+    User registerUser(User user);
+
+    User findByEmailIdAndPassword(String emailId, String password);
+
+    User findByEmailId(String emailId);
+    List<User> getAllUsers();
+
+    boolean deleteUser(Long id);
+
+    boolean updateUser(Long id, User updatedUser);
+
+    User getUserById(Long id);
+}
+
