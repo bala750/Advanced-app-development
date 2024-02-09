156c107c7c4e93d21f94e94175c086eaad7d8f14
bala750 <122767885+bala750@users.noreply.github.com>
2024-02-09 10:31:55 +0530

    Add files via upload

---
+++ Backend/day 9/src/main/java/com/example/springapp/entity/LoginRequest.java
@@ -0,0 +1,32 @@
+package com.example.springapp.entity;
+
+public class LoginRequest {
+
+    private String emailId;
+    private String password;
+
+    // Constructors
+
+    public LoginRequest() {
+    }
+
+    public LoginRequest(String emailId, String password) {
+        this.emailId = emailId;
+        this.password = password;
+    }
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
